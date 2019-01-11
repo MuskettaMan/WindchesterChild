5c65a406fdd17fdd6f6a5a8e6b4653438a4f0cfe
DESKTOP-1STIJ1S\bradl <thekid72gaming@gmail.com>
2019-01-11 12:25:13 +0100

    added movement script and test art

---
+++ Windchester Child/Assets/BradleyScripts/InputManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public class inputManager
+    {
+
+        private bool w = false;
+        private bool a = false;
+        private bool s = false;
+        private bool d = false;
+        private bool space = false;
+        private bool k = false;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            checkKeycodes();
+
+        }
+
+
+        public bool W
+        {
+            get { return w; }
+        }
+        public bool A
+        {
+            get { return a; }
+        }
+        public bool S
+        {
+            get { return s; }
+        }
+
+        public bool D
+        {
+            get { return d; }
+        }
+        public bool K
+        {
+            get { return k; }
+        }
+        public bool Space
+        {
+            get { return space; }
+        }
+
+
+        public void checkKeycodes()
+        {
+            if (Input.GetKey(KeyCode.W)) { w = true; } else w = false;
+            if (Input.GetKey(KeyCode.A)) { a = true; } else a = false;
+            if (Input.GetKey(KeyCode.S)) { s = true; } else s = false;
+            if (Input.GetKey(KeyCode.D)) { d = true; } else d = false;
+            if (Input.GetKey(KeyCode.Space)) { space = true; } else space = false;
+            if (Input.GetKey(KeyCode.K)) { k = true; } else k = false;
+
+        }
+    }
+}
---
+++ Windchester Child/Assets/BradleyScripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 1;
+  
+    void Update()
+    {
+        Movement();
+    }
+    void Movement()
+    {
+        if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)))
+        {
+            
+            Vector2 move = transform.position += Vector3.up *speed;
+            GetComponent<Rigidbody2D>().MovePosition(move * Time.deltaTime);
+
+        }
+
+        if ((Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)))
+        {
+            Vector2 move = transform.position += Vector3.down * speed;
+            GetComponent<Rigidbody2D>().MovePosition(move * Time.deltaTime);
+
+        }
+        if ((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)))
+        {
+            Vector2 move = transform.position += Vector3.right * speed;
+            GetComponent<Rigidbody2D>().MovePosition(move * Time.deltaTime);
+
+        }
+        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)))
+        {
+            Vector2 move = transform.position += Vector3.left * speed;
+            GetComponent<Rigidbody2D>().MovePosition(move * Time.deltaTime);
+
+        }
+    }
+}
